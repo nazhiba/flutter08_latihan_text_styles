3119563a2287508af1a50946c269ab509587d1bc
Nadzib <nadzib22@gmail.com>
2024-03-21 20:47:52 +0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -4,4 +4,20 @@ void main() {
   runApp(const MyApp());
 }
 
-stl
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+        title: Text("Latihan Text Style"),
+      ),
+      body: Center(
+        child: Text("Ini adalah Text"),
+      ),
+      ),
+    );
+  }
+}
